8aa367dccd74bdda421aeb370b4b31cd39515bbf
Marika Lee <marika.lee@rakuten.com>
2016-09-24 12:32:49 -0700

    fix changes

--- app.py
+++ app.py
@@ -22,14 +22,8 @@ def hello():
     flightcode=request.form['yourflightcode']
     airline=request.form['yourairline']
     flightnumber=request.form['yourflightnumber']
-    singaporean=request.form['yoursingaporean']
 
-    return render_template('form_action.html', name=name, email=email, flightcode=flightcode, airline=airline, flightnumber=flightnumber, singaporean=singaporean)
-
-# Define a route for the default URL, which loads the form
-@app.route('/checkbox/', methods=['GET'])
-def checkbox():
-    return render_template('checkbox_action.html')
+    return render_template('form_action.html', name=name, email=email, flightcode=flightcode, airline=airline, flightnumber=flightnumber)
 
 @app.route('/checkbox/', methods=['GET'])
 def checkbox():
